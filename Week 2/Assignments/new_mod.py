a7a633508cfe9d4065281c9c12575d13c561ea88
andrewapeter801 <144055669+andrewapeter801@users.noreply.github.com>
2023-09-21 06:47:45 -0600

    assignments/notes

---
+++ Week 2/Assignments/new_mod.py
@@ -0,0 +1,8 @@
+def caluculate_square_footage(length, width):
+    return length * width
+    
+def calculate_circumference(radius):
+    return radius * 3.14159265359 * radius
+
+def calculate_feet_to_inches(feet):
+    return feet * 12
---
+++ Week 2/Class Notes/test129.py/module.py
@@ -0,0 +1,11 @@
+def print_name(name):
+    '''
+    Funtion name: print_name
+    Input: Expects a string value representing the name the user wishes to print
+    Output: A string value that is a sentence welcoming the user
+
+    Overall, we take in a single string value and insert it into a f-string to return a sentence welcoming a user.
+    '''
+    return f'Hello {name}! It is great to see you! Welcome to the Bonfire 129 classroom! We are happy to have you'
+    
+    
